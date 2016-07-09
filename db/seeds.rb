d846ea0d6520e9e7d02b8243ebae90c19bc6ced4
sqacecadet <sqacecadet@gmail.com>
2016-07-08 22:29:32 -0300

    removing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-documents = Documents.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-Character.create(name: 'Luke', movie: movies.first)
-
-movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-Character.create(name: 'Luke', movie: movies.first)
